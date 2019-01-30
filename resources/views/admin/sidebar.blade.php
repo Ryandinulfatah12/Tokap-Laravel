465bbe1ec43c145fcf1b0d83b40d53dd962d429a
Ryandinulfatah12 <ryandfbasketball12@gmail.com>
2019-01-29 23:56:34 -1200

    pemindahan sourcecode navbar,footer,sidebar

---
+++ resources/views/admin/sidebar.blade.php
@@ -0,0 +1,8 @@
+<ul class="sidebar navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="{{route('admin.home')}}">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Dashboard</span>
+          </a>
+        </li>
+</ul>
