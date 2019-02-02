c37584e19a31b4c5411acb1483e9638a7ca89a3a
Ryandinulfatah12 <ryandfbasketball12@gmail.com>
2019-02-01 22:54:33 -1200

    menambahkan fungsi redirectTo ke admin.home

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -25,7 +25,7 @@ class ResetPasswordController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+
 
     /**
      * Create a new controller instance.
@@ -35,5 +35,9 @@ class ResetPasswordController extends Controller
     public function __construct()
     {
         $this->middleware('guest');
+    }
+    protected function redirectTo()
+    {
+        return route('admin.home');
     }
 }
