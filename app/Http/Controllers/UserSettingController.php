eca0d3942156ad732681e675c0d91721cbfb9568
Ryandinulfatah12 <ryandfbasketball12@gmail.com>
2019-02-05 23:48:14 -1200

    menambahkan user setting validate

--- app/Http/Controllers/UserSettingController.php
+++ app/Http/Controllers/UserSettingController.php
@@ -10,11 +10,21 @@ class UserSettingController extends Controller
 {
     public function form()
     {
+    	
     	$data = User::where('id',Auth::id())->first();
     	return view('admin.pages.user.setting',['dt'=>$data]);
     }
-    public function update()
+    
+    public function update(Request $req)
     {
+    	$id = Auth::id();
+    	\Validator::make($req->all(), [
+    			'name'=>'required|between:3,100',
+    			'email'=>'required|email|unique:users,email,'.$id,
+    			'password'=>'nullable|min:6',
+    			'repassword'=>'same:password',
+    		])->validate();
+
     	return "Fungsi Update";
     }
 }
