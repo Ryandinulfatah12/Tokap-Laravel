62f52d28cd2bb0167907f9edd5ad694ad25f5ebd
Ryandinulfatah12 <ryandfbasketball12@gmail.com>
2019-02-05 23:50:37 -1200

    penambahan url pada form control

--- resources/views/admin/pages/user/setting.blade.php
+++ resources/views/admin/pages/user/setting.blade.php
@@ -11,25 +11,34 @@
 				<div class="card-body">
 					{{csrf_field()}}
 					<div class="form-group form-label-group">
-						<input type="text" name="name" class="form-control" value="{{ old('name',$dt->name) }}"
+						<input type="text" name="name"
+						 class="form-control {{$errors->has('name')?'is-invalid':''}}" value="{{ old('name',$dt->name) }}"
 						id="iName" placeholder="Name" required>
 						<label for="iName">Name</label>
+						@if($errors->has('name'))
+						<div class="invalid-feedback">{{$errors->first('name')}}</div>
+						@endif
 					</div>
 
 					<div class="form-group form-label-group">
 						<input type="text" name="email"
-						class="form-control"
+						class="form-control {{$errors->has('email')?'is-invalid':''}}"
 						value=" {{ old('email',$dt->email) }} "
 						id="iEmail" placeholder="Email" required>
 						<label for="iEmail">Email</label>
+						@if($errors->has('email'))
+						<div class="invalid-feedback">{{$errors->first('email')}}</div>
+						@endif
 					</div>
 
 					<div class="form-group form-label-group">
 						<input type="password" name="password"
-						class="form-control"
-						value=" {{ old('password',$dt->password) }} "
+						class="form-control {{$errors->has('password')?'is-invalid':''}}"
 						id="iPassword" placeholder="Password" required>
 						<label for="iPassword">Password</label>
+						@if($errors->has('password'))
+						<div class="invalid-feedback">{{$errors->first('password')}}</div>
+						@endif
 						<div class="form-text text-muted">
 							<small>Kosongkan Password Apabila Tidak Diubah</small>
 						</div>
@@ -37,9 +46,12 @@
 
 					<div class="form-group form-label-group">
 						<input type="password" name="repassword"
-						class="form-control"
+						class="form-control {{$errors->has('repassword')?'is-invalid':''}}"
 						id="iRePassword" placeholder="Re Password">
 						<label for="iRePassword">Re Password</label>
+						@if($errors->has('repassword'))
+						<div class="invalid-feedback">{{$errors->first('repassword')}}</div>
+						@endif
 					</div>
 				</div>
 				<div class="card-footer">
