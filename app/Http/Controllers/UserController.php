be56f13deee504db114417dcafec52b64135501b
Ryandinulfatah12 <ryandfbasketball12@gmail.com>
2019-02-13 10:29:20 -1200

    membuat fungsi validator di form tambah user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -21,6 +21,15 @@ class UserController extends Controller
 
 	public function save(Request $req)
 	{
+
+		\Validator::make($req->all(),[
+			'name'=>'required|between:3,100',
+			'email'=>'required|unique:users,email',
+			'password'=>'required|min:6',
+			'repassword'=>'required|same:password',
+			'akses'=>'required',
+			])->validate();
+
 		return 'Fungsi Save';
 	}
 }
