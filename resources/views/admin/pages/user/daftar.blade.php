4734cca391ce497bbf5dec0d4b8482836dab9735
Ryandinulfatah12 <ryandfbasketball12@gmail.com>
2019-02-16 01:46:00 -1200

    memberi url ke form edit pada tombol edit

--- resources/views/admin/pages/user/daftar.blade.php
+++ resources/views/admin/pages/user/daftar.blade.php
@@ -46,7 +46,7 @@
 		<td>{{ $dt->email }}</td>
 		<td>{{ $dt->akses }}</td>
 		<td>
-			<a href="#" class="btn btn-success btn-sm">
+			<a href="{{ route('admin.user.edit',['id'=>$dt->id]) }}" class="btn btn-success btn-sm">
 				<i class="fa fa-w fa-edit"></i>
 			</a>
 			@if($dt->id !=Auth::id())
