cf7d4dd6fb2f67fe4c169ba9bd47fb901d6e55c1
Ryandinulfatah12 <ryandfbasketball12@gmail.com>
2019-02-18 00:27:50 -1200

    menambahkan fungsi delete database

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -87,4 +87,16 @@ class UserController extends Controller
 
 	}
 
+	public function delete(Request $req)
+	{
+		$result = User::find($req->id);
+
+		if ($result->delete()) {
+			return back()->with('result','delete');
+		} else {
+			return  back()->with('result','fail-delete');
+		}
+		
+	}
+
 }
