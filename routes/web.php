39ef4111f3dea1fe18b998d7de11a9ef1fcdb86a
Ryandinulfatah12 <ryandfbasketball12@gmail.com>
2019-02-22 10:38:58 -1200

    menambah route baru kategori

--- routes/web.php
+++ routes/web.php
@@ -15,13 +15,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['middleware'=>['auth']], function(){
-	Route::prefix('admin')->group(function(){
-		Route::get('/', function(){
-			return view('admin.pages.dashboard');
-		})->name('admin.home');
 
-		Route::prefix('user')->group(function(){
+Route::group(['prefix'=>'admin','middleware'=>['auth']], function(){
+	
+	Route::get('/', function(){
+	return view('admin.pages.dashboard');
+	})->name('admin.home');
+	
+	/* User */
+	Route::prefix('user')->group(function(){
 
 			Route::get('/','UserController@daftar')->name('admin.user')->middleware('akses.admin');
 			Route::delete('/','UserController@delete')->middleware('akses.admin');
@@ -34,7 +36,10 @@ Route::group(['middleware'=>['auth']], function(){
 
 			Route::get('/setting','UserSettingController@form')->name('admin.user.setting');
 			Route::post('/setting','UserSettingController@update');
-		});
+	});
+	/* User */
+	Route::group(['prefix'=>'kategori','middleware'=>'akses.admin'], function(){
+		Route::get('/','KategoriController@daftar')->name('admin.kategori');
 	});
 });
 
