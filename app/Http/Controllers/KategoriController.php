7e3d68daa500d04c6339cfa38afb9223137ff174
Ryandinulfatah12 <ryandfbasketball12@gmail.com>
2019-02-23 10:33:57 -1200

    menambah fungsi add,save serta validasi nya

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -14,4 +14,18 @@ class KategoriController extends Controller
     	return view('admin.pages.kategori.daftar',['data'=>$data]);
 
     }
+
+    public function add()
+    {
+    	return view('admin.pages.kategori.add');
+    }
+
+    public function save(Request $req)
+    {
+    	\Validator::make($req->all(),[
+    		'kategori'=>'required|between:3,100|unique:kategori,nama_kategori'
+    	])->validate();
+    	return 'Fungsi Save';
+    }
+
 }
