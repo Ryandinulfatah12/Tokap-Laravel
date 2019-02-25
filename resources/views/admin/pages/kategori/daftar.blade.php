9c5be6ff906d4e6f37332eb0476df7ae733c14c2
Ryandinulfatah12 <ryandfbasketball12@gmail.com>
2019-02-25 10:42:36 -1200

    memberi url edit ke form edit kategori

--- resources/views/admin/pages/kategori/daftar.blade.php
+++ resources/views/admin/pages/kategori/daftar.blade.php
@@ -44,7 +44,8 @@
 	<tr>
 		<td>{{ $dt->nama_kategori }}</td>
 		<td>
-			<a href="#" class="btn btn-success btn-sm">
+			<a href="{{route('admin.kategori.edit',['id'=>$dt->id])}}" 
+				class="btn btn-success btn-sm">
 				<i class="fa fa-w fa-edit"></i>
 			</a>
 
