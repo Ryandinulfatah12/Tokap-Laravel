671e73b3f262f580f3ed050465a57a9251667c37
Ryandinulfatah12 <ryandfbasketball12@gmail.com>
2019-02-25 10:44:16 -1200

    menambah fungsi edit dan update

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -36,8 +36,18 @@ class KategoriController extends Controller
     	} else {
     		return back()->with('result','fail')->withInput();
     	}
-    	
 
+    }
+
+    public function edit($id)
+    {
+        $data = Kategori::where('id',$id)->first();
+        return view('admin.pages.kategori.edit',['rc'=>$data]);
+    }
+
+    public function update(Request $req)
+    {
+        return 'Fungsi Update';
     }
 
 }
